886a1b2de6e379688211e970df9f617bf114ca63
Blake Duncan <blakecduncan@gmail.com>
2023-11-15 15:32:45 +0300

    Remove console log

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,7 +12,6 @@ function App() {
   const { wallet: activeWallet, setActiveWallet } = usePrivyWagmi();
 
   useEffect(() => {
-    console.log('blake')
     if (wallets[0] && !activeWallet) {
         setActiveWallet(wallets[0]);
     }
